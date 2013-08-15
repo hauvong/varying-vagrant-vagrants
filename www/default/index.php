8025a0b7615570b260778f9777dee964868e4243
Gabriel Koen <gabriel.koen@gmail.com>
2013-08-15 12:52:18 -0700

    Adding site links to dashboard

--- www/default/index.php
+++ www/default/index.php
@@ -6,13 +6,31 @@
 </head>
 <body>
 <ul class="nav">
-	<li class="active"><a href="#">Home</a></li>
+	<li class="active"><h2>Tools</h2></li>
 	<li><a href="https://github.com/Penske-Media-Corp/varying-vagrant-vagrants">PMC VVV Home</a></li>
 	<li><a href="database-admin/">phpMyAdmin</a></li>
 	<li><a href="memcached-admin/">phpMemcachedAdmin</a></li>
 	<li><a href="webgrind/">Webgrind</a></li>
 	<li><a href="apc/">APC</a><br />Username: apc<br />Password: apc</li>
 	<li><a href="phpinfo/">PHP Info</a></li>
+</ul>
+
+<ul class="nav">
+	<li class="active"><h2>Sites</h2></li>
+	<li><a href="http://awardsline.dev/">Awardsline</a><br />
+		<a href="http://awardsline.dev/wp-admin/">Awardsline Admin</a></li>
+	<li><a href="http://bgr.dev/">BGR</a><br />
+		<a href="http://bgr.dev/wp-admin/">BGR Admin</a></li>
+	<li><a href="http://deadline.dev/">Deadline</a><br />
+		<a href="http://deadline.dev/wp-admin/">Deadline Admin</a></li>
+	<li><a href="http://hollywoodlife.dev/">HollywoodLife</a><br />
+		<a href="http://hollywoodlife.dev/wp-admin/">HollywoodLife Admin</a></li>
+	<li><a href="http://tvline.dev/">TVLine</a><br />
+		<a href="http://tvline.dev/wp-admin/">TVLine Admin</a></li>
+	<li><a href="http://variety.dev/">Variety</a><br />
+		<a href="http://variety.dev/wp-admin/">Variety Admin</a></li>
+	<li><a href="http://variety411.dev/">Variety411</a><br />
+		<a href="http://variety411.dev/wp-admin/">Variety411 Admin</a></li>
 </ul>
 </body>
 </html>
